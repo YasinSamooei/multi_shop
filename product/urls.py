c3137f28d485c0f84626ffe99a7bed3f51dc9968
YasinSamooei <110273854+YasinSamooei@users.noreply.github.com>
2023-07-22 18:27:51 +0430

    add product filter

--- product/urls.py
+++ product/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name="product"
 
 urlpatterns=[
-    path("<int:pk>",views.ProductDetailView.as_view(),name="product-detail")
+    path("<int:pk>",views.ProductDetailView.as_view(),name="product-detail"),
+    path("list",views.ProductsListView.as_view(),name="products-list")
 ]
--- product/views.py
+++ product/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView,ListView
 from .models import Product
+
 class ProductDetailView(DetailView):
     template_name="product/detail.html"
     model=Product
+
+
+class ProductsListView(ListView):
+    template_name='product/products_list.html'
+    queryset=Product.objects.all()
+
+    def get_context_data(self, **kwargs):
+        request= self.request
+        colors=request.GET.getlist('color')
+        sizes=request.GET.getlist('size')
+        min_price=request.GET.get('min_price')
+        max_price=request.GET.get('max_price')
+
+        queryset=Product.objects.all()
+        if colors:
+            queryset=queryset.filter(Color__title__in=colors).distinct()
+        if sizes:
+            queryset=queryset.filter(size__title__in=sizes).distinct()
+        if min_price and max_price:
+            queryset=queryset.filter(price__lte=max_price,price__gte=min_price).distinct()
+
+        context= super(ProductsListView,self).get_context_data()
+        context['object_list']=queryset
+        return context
